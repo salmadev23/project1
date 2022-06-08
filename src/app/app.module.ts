ec2558914718a87ef98d8e68742b0e8ccbff77f6
salmadev23 <mohammadsalma023@gmail.com>
2022-06-08 16:29:52 +0530

    salma

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutUsComponent } from './about-us/about-us.component';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
+import { HomeComponent } from './components/home/home/home.component';
+import { ProductsComponent } from './components/products/products/products.component';
+import { BlogsComponent } from './components/blogs/blogs/blogs.component';
+import { MyPipePipe } from './core/pipes/my-pipe.pipe';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutUsComponent
+    AboutUsComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ProductsComponent,
+    BlogsComponent,
+    MyPipePipe,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/home/home/home.component.spec.ts
+++ src/app/components/home/home/home.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AboutUsComponent } from './about-us.component';
+import { HomeComponent } from './home.component';
 
-describe('AboutUsComponent', () => {
-  let component: AboutUsComponent;
-  let fixture: ComponentFixture<AboutUsComponent>;
+describe('HomeComponent', () => {
+  let component: HomeComponent;
+  let fixture: ComponentFixture<HomeComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AboutUsComponent ]
+      declarations: [ HomeComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(AboutUsComponent);
+    fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
